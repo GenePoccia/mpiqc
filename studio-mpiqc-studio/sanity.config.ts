9658630db656d8aa0ce6419250f8971efc52acba
Eugenio Poccia <pocciaeugenio@gmail.com>
2025-02-11 00:34:04 -0500

    feat: setup staging/production datasets and environment variables

--- studio-mpiqc-studio/sanity.config.ts
+++ studio-mpiqc-studio/sanity.config.ts
@@ -2,17 +2,56 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {
+  dashboardTool,
+  sanityTutorialsWidget,
+  projectUsersWidget,
+  projectInfoWidget,
+} from '@sanity/dashboard'
 
-export default defineConfig({
-  name: 'default',
-  title: 'mpiqc-studio',
+export default defineConfig([
+  {
+    name: 'default',
+    title: 'mpiqc-studio-production',
 
-  projectId: 'x8gnacpf',
-  dataset: 'production',
+    projectId: process.env.SANITY_STUDIO_TITLE_PROJECT_ID!,
+    dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+    basePath: '/production',
+    subtitle: 'production',
 
-  schema: {
-    types: schemaTypes,
+    plugins: [
+      structureTool(),
+      visionTool(),
+      dashboardTool({
+        widgets: [sanityTutorialsWidget(), projectInfoWidget(), projectUsersWidget()],
+      }),
+    ],
+
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    name: 'staging',
+    title: 'mpiqc-studio-staging',
+
+    projectId: process.env.SANITY_STUDIO_TITLE_PROJECT_ID!,
+    dataset: 'staging',
+
+    basePath: '/staging',
+    subtitle: 'staging',
+
+    plugins: [
+      structureTool(),
+      visionTool(),
+      dashboardTool({
+        widgets: [sanityTutorialsWidget(), projectInfoWidget(), projectUsersWidget()],
+      }),
+    ],
+
+    schema: {
+      types: schemaTypes,
+    },
+  },
+])

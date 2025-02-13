62be32ba467d597976ae2566ffaf3d3ddc8b0ea7
Eugenio Poccia <pocciaeugenio@gmail.com>
2025-02-13 00:46:13 -0500

    feat: remove comments test

--- mpiqc-frontend/src/app/page.tsx
+++ mpiqc-frontend/src/app/page.tsx
@@ -1,12 +1,4 @@
-// import Image from "next/image";
-// import styles from "./page.module.css";
 import "./globals.css";
-
-// import Link from "next/link";
-// import { type SanityDocument } from "next-sanity";
-
-// import { client } from "@/sanity/client";
-// import { options } from "../lib/sanityOptions";
 
 export default async function Home() {
 	return (

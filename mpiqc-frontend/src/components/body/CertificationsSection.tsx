52779f5221777f85af98bb98a5ff46f46ef974dc
Eugenio Poccia <pocciaeugenio@gmail.com>
2025-02-19 10:15:08 -0500

    feat: certifications

--- mpiqc-frontend/src/components/body/CertificationsSection.tsx
+++ mpiqc-frontend/src/components/body/CertificationsSection.tsx
@@ -1,5 +1,48 @@
 import "../../app/globals.css";
 
+const logos = [
+	{
+		id: "logo-1",
+		description: "Logo 1",
+		image: "https://shadcnblocks.com/images/block/logos/astro.svg",
+		className: "h-7 w-auto",
+	},
+	{
+		id: "logo-2",
+		description: "Logo 2",
+		image: "https://shadcnblocks.com/images/block/logos/figma.svg",
+		className: "h-7 w-auto",
+	},
+	{
+		id: "logo-3",
+		description: "Logo 3",
+		image: "https://shadcnblocks.com/images/block/logos/nextjs.svg",
+		className: "h-7 w-auto",
+	},
+];
+
 export default async function Certifications() {
-	return <section id="certifications">Certifications</section>;
+	return (
+		<section
+			id="certifications"
+			className="my-24"
+		>
+			<h3>Certifications</h3>
+			<div className="flex flex-wrap justify-center items-center gap-x-8 gap-y-4 mt-12">
+				{logos.map((logo) => (
+					<div key={logo.id}>
+						<div>
+							<div>
+								<img
+									src={logo.image}
+									alt={logo.description}
+									className={logo.className}
+								/>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</section>
+	);
 }

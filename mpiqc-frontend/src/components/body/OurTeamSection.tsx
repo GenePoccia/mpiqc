77dc6083c2b528fe21feafe15aeaaf2d6e2874bb
Eugenio Poccia <pocciaeugenio@gmail.com>
2025-02-19 16:55:09 -0500

    feat: carousel init

--- mpiqc-frontend/src/components/body/OurTeamSection.tsx
+++ mpiqc-frontend/src/components/body/OurTeamSection.tsx
@@ -1,5 +1,38 @@
 import "../../app/globals.css";
 
+import { Card, CardContent } from "@/components/ui/card";
+
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+
 export default async function OurTeam() {
-	return <section id="our-team">Our Team</section>;
+	return (
+		<section
+			id="our-team"
+			className="flex justify-center"
+		>
+			<Carousel className="w-full w-3xl m-height-md">
+				<CarouselContent>
+					{Array.from({ length: 5 }).map((_, index) => (
+						<CarouselItem key={index}>
+							<div className="p-1">
+								<Card>
+									<CardContent className="flex aspect-[16/9] items-center justify-center p-6">
+										<span className="text-4xl font-semibold">{index + 1}</span>
+									</CardContent>
+								</Card>
+							</div>
+						</CarouselItem>
+					))}
+				</CarouselContent>
+				<CarouselPrevious />
+				<CarouselNext />
+			</Carousel>
+		</section>
+	);
 }

d736ee8b1dc9edce8dbc9aaf9fc4dd02afeb3d29
Eugenio Poccia <pocciaeugenio@gmail.com>
2025-02-18 21:05:16 -0500

    feat: fix overflow

--- mpiqc-frontend/src/app/layout.tsx
+++ mpiqc-frontend/src/app/layout.tsx
@@ -22,17 +22,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
 	children,
-  }: Readonly<{ children: React.ReactNode }>) {
+}: Readonly<{ children: React.ReactNode }>) {
 	return (
-	  <html lang="en">
-		<body className={`${geistSans.variable} ${geistMono.variable}`}>
-		  <div className="container">
-
-			<Header />
-			{children}
-			<Footer />
-		  </div>
-		</body>
-	  </html>
+		<html lang="en">
+			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+				<div className="container">
+					<Header />
+					{children}
+					<Footer />
+				</div>
+			</body>
+		</html>
 	);
-  }
+}
--- mpiqc-frontend/src/components/body/WelcomeSection.tsx
+++ mpiqc-frontend/src/components/body/WelcomeSection.tsx
@@ -1,5 +1,20 @@
-import "../../app/globals.css";
-
 export default async function WelcomeSection() {
-	return <section id="home">Welcome Section</section>;
+	return (
+		<section
+			id="home"
+			className="relative min-h-screen p-6"
+		>
+			<div className="absolute text-left bottom-32 left-6 space-y-4 max-w-[90%] sm:max-w-[70%]">
+				<h1 className="text-4xl font-bold sm:text-3xl">We set the standard.</h1>
+				<p className="border-l-2 border-white pl-6">
+					MPIQC is a premiere provider of a comprehensive suite of contract
+					manufacturing solutions and services.
+					<br />
+					As a team of professionals, we treat our customers as partners while
+					remaining focused on delivering high-quality, high-value, flexible CM
+					solutions our customers have come to demand.
+				</p>
+			</div>
+		</section>
+	);
 }

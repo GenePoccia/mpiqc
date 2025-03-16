ebc8271bbdef270a07d8feab152be9e43f7a4cc2
Eugenio Poccia <pocciaeugenio@gmail.com>
2025-03-15 21:44:05 -0400

    chore: last linting

--- mpiqc-frontend/src/components/reusable/PortableTextComponents.tsx
+++ mpiqc-frontend/src/components/reusable/PortableTextComponents.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import {
 	PortableTextReactComponents,
 	PortableTextComponentProps,
-	PortableTextMarkComponentProps,
 	PortableTextBlock,
 } from "@portabletext/react";
 

377f147fdae83894a316e1c8f0830b7b695cb3f6
Eugenio Poccia <pocciaeugenio@gmail.com>
2025-03-15 21:42:24 -0400

    chore: linting?

--- mpiqc-frontend/src/components/reusable/PortableTextComponents.tsx
+++ mpiqc-frontend/src/components/reusable/PortableTextComponents.tsx
@@ -3,40 +3,43 @@ import {
 	PortableTextReactComponents,
 	PortableTextComponentProps,
 	PortableTextMarkComponentProps,
+	PortableTextBlock,
 } from "@portabletext/react";
 
+// Define the components
 export const PortableTextComponents: Partial<PortableTextReactComponents> = {
 	block: {
-		normal: ({ children }: PortableTextComponentProps<any>) => (
+		normal: ({ children }: PortableTextComponentProps<PortableTextBlock>) => (
 			<p className="text-base leading-relaxed">{children}</p>
 		),
 	},
 	list: {
-		bullet: ({ children }: PortableTextComponentProps<any>) => (
+		bullet: ({ children }: PortableTextComponentProps<PortableTextBlock>) => (
 			<ul className="list-disc pl-5">{children}</ul>
 		), // ✅ Bulleted list
-		number: ({ children }: PortableTextComponentProps<any>) => (
+		number: ({ children }: PortableTextComponentProps<PortableTextBlock>) => (
 			<ol className="list-decimal pl-5">{children}</ol>
 		), // ✅ Numbered list
 	},
 	listItem: {
-		bullet: ({ children }: PortableTextComponentProps<any>) => (
+		bullet: ({ children }: PortableTextComponentProps<PortableTextBlock>) => (
 			<li className="mb-1">{children}</li>
 		), // ✅ Each bullet item
-		number: ({ children }: PortableTextComponentProps<any>) => (
+		number: ({ children }: PortableTextComponentProps<PortableTextBlock>) => (
 			<li className="mb-1">{children}</li>
 		),
 	},
 	marks: {
-		strong: ({ children }: PortableTextMarkComponentProps<any>) => (
+		strong: ({ children }) => (
 			<strong className="font-normal text-black">{children}</strong> // Apply styling for strong text
 		),
 	},
 };
 
+// Header component for Portable Text rendering
 export const HeaderPortableText = {
 	block: {
-		normal: ({ children }: PortableTextComponentProps<any>) => (
+		normal: ({ children }: PortableTextComponentProps<PortableTextBlock>) => (
 			<h1 className="text-[64px] w-[90%] font-bold sm:text-3xl !text-white mb-4 !leading-[120%]">
 				{children}
 			</h1>
